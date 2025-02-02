b13ffdc6141857cd52ded4eba243dd31b88a5c01
sidrajawaid <sidra.jawaid00@hotmail.com>
2025-02-02 14:01:03 -0500

    First commit

---
+++ app/src/main/java/com/example/animatedsplashscreentutorial/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.animatedsplashscreentutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.animatedsplashscreentutorial.ui.theme.AnimatedSplashScreenTutorialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AnimatedSplashScreenTutorialTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AnimatedSplashScreenTutorialTheme {
+        Greeting("Android")
+    }
+}
